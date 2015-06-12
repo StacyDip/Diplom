9fc98ef6174e0d4863269cba1b6cc8ac273bb589
Stacy <Stacy@hp>
2015-06-13 01:24:42 +0300

    валидация параметров

--- src/Logic/ValidationParam.java
+++ src/Logic/ValidationParam.java
@@ -11,19 +11,58 @@ package Logic;
  */
 public class ValidationParam {
 
+    /**
+     *
+     * @param polinom
+     * @return если вид полинома верный
+     */
     public boolean chekPolinomMSeq(int[] polinom) {
+
+        if (polinom[0] != 1) {
+            return false;
+
+        }
+
+        for (int i = 0; i < polinom.length; i++) {
+
+            if ((polinom[i] != 0) || (polinom[i] != 1)) {
+
+                return false;
+            }
+
+        }
+
         return true;
     }
 
     public boolean chekSizeDecMatx(int rowMatrxDec, int colMartxDec, int periodGMW) {
+
+        int temp = rowMatrxDec * colMartxDec;
+        if (temp != periodGMW) {
+            return false;
+        }
         return true;
     }
 
     public boolean chekFormatВasicSequence(int[] basicSequence) {
+        for (int i = 0; i < basicSequence.length; i++) {
+
+            if ((basicSequence[i] != 0) || (basicSequence[i] != 1)) {
+
+                return false;
+            }
+
+        }
+
         return true;
     }
 
     public boolean chekSizeВasicSequence(int colMartxDec, int[] basicSequence) {
+
+        if (basicSequence.length != colMartxDec) {
+            return false;
+        }
+
         return true;
     }
 }
